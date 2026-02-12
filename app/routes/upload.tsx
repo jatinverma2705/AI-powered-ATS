2c8db4167c2e4d6113053e2ab016e94c04a3c9cf
Jatin Verma <jatinverma2705@gmail.com>
2026-02-12 23:15:55 +0530

    Implemented resume feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -68,7 +68,7 @@ const upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data))
         setStatusText('Analysis complete , redirecting....')
         console.log(data);
-        navigate('/')
+        navigate(`/resume/${uuid}`)
     } 
 
     const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
